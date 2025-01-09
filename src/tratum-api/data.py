4b9d7562a474f53ba3a74e83c072ee99f1445c1c
Lucas Barbosa <lucas.barbosa@murabei.com>
2025-01-09 11:02:16 -0300

    ♻️ changed token generation to __init__

--- src/tratum-api/data.py
+++ src/tratum-api/data.py
@@ -2,36 +2,23 @@
 import requests
 
 class TratumAPI:
-    def __init__(self, tratum_username: str, tratum_password: str):
-        """__init__
+    def __init__(self, tratum_email: str, tratum_password: str):
+        """__init__.
 
         Args:
-            tratum_username (str): Username for Tratum API.
+            tratum_email (str): E-mail for Tratum API.
             tratum_password (str): Password for Tratum API.
-        """
-        self.tratum_username = tratum_username
-        self.tratum_password = tratum_password
-
-    def get_token(self, email: str, password: str):
-        """Generate API token.
-
-        Args:
-            email (str): Tratum API account e-mail.
-            password (str): Tratum API account password.
-
-        Returns:
-            str: API token.
         """
         url = "https://search.tratum.com.br/v1/login"
         headers = {
             "Content-Type": "application/json"
         }
         data = {
-            "email": email,
-            "password": password,
+            "email": tratum_email,
+            "password": tratum_password,
         }
         response = requests.post(url, headers=headers, json=data)
-        return response.json()['token']
+        self.token = response.json()['token']
 
     def get_process_detail(
         self, token: str, organization_id: str, process_number: str):
@@ -40,6 +27,6 @@ class TratumAPI:
             process_number=process_number,
         )
         headers = {
-            'Authorization': 'Bearer {token}'.format(token=token)
+            'Authorization': 'Bearer {token}'.format(token=self.token)
         }
         response = requests.get(url, headers=headers)
